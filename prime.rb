d5a12cec22be3a3e5d3c467fa1a05a6b5e723e4a
Collin Jilbert <cjilbert504@gmail.com>
2019-11-12 19:49:14 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,7 @@
-def prime?(n)
-  if n <= 1
-    return false
-  elsif n <= 3
-    return true
-  else (2..n/2).none? do |x|
-    n % x == 0
+def prime?(num)
+  (2..(num - 1)).each do |n|
+    return false if num % n == 0
   end
-  end
+  true
 end
+  
